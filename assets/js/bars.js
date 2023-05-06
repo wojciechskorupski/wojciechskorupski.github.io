f64f7f63a8b3a1bcb3804677f0c6ba05ba17cd5e
Wojciech Skorupski <wojciechskorupski91@gmail.com>
2023-05-06 21:02:57 +0200

    bars edit

--- assets/js/bars.js
+++ assets/js/bars.js
@@ -1,7 +1,13 @@
 jQuery(document).ready(function() {
-  jQuery('.skillbar').each(function() {
-    jQuery(this).find('.skillbar-bar').animate({
-      width: jQuery(this).attr('data-percent')
-    }, 4000);
-  });
+	var el = document.getElementById("trigger");
+	jQuery(window).on('scroll', function() {		  
+	  if ( el.offsetTop <= document.documentElement.scrollTop + window.innerHeight / 2 ) {
+				  jQuery('.skillbar').each(function() {
+			jQuery(this).find('.skillbar-bar').animate({
+			  width: jQuery(this).attr('data-percent')
+			}, 4000);
+		  });
+
+	  }
+	})
 });
